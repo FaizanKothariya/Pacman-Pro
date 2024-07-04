35bce0199e5400c44d059f4ca0611f0a0699f5e9
Faizan Kothariya <kothariyafaizan2001@gmail.com>
2024-07-04 12:13:11 +0530

    Pacman Character Adding

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("canvas");
 const canvasContext = canvas.getContext("2d");
 
-const pacmanFrames = document.getElementById("animation");
+const pacmanFrames = document.getElementById("animations");
 const ghostFrames = document.getElementById("ghosts");
 
 let createRect = (x, y, width, height, color) => {
@@ -15,6 +15,8 @@ let wallColor = "#342DCA";
 let wallSpaceWidth = oneBlockSize / 1.5;
 let wallOffset = (oneBlockSize - wallSpaceWidth) / 2;
 let wallInnerColor = "black";
+let foodColor = "#FEB897"
+let score = 0;
 
 const DIRECTION_RIGHT = 4;
 const DIRECTION_UP = 3;
@@ -30,7 +32,7 @@ let map = [
   [1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1],
   [1, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 1],
   [1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 1, 2, 1, 1, 1, 2, 1, 1, 1, 1, 1],
-  [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 1, 1, 2, 1, 0, 0, 0, 0],
+  [0, 0, 0, 0, 1, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1, 2, 1, 0, 0, 0, 0],
   [1, 1, 1, 1, 1, 2, 1, 2, 1, 1, 2, 1, 1, 2, 1, 2, 1, 1, 1, 1, 1],
   [2, 2, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 2, 1],
   [1, 1, 1, 1, 1, 2, 1, 2, 1, 2, 2, 2, 1, 2, 1, 2, 1, 1, 1, 1, 1],
@@ -54,12 +56,31 @@ let gameLoop = () => {
 
 let update = () => {
   // todo
+
+  pacman.moveProcess();
+  pacman.eat()
 };
 
+let drawFoods = () => {
+  for (let i = 0; i < map.length; i++) {
+       for( let j = 0; j < map[0].length; j++){
+        if(map[i][j] == 2){
+          createRect(j* oneBlockSize + oneBlockSize / 3,
+            i * oneBlockSize + oneBlockSize / 3,
+            oneBlockSize/ 3,
+            oneBlockSize/ 3,
+            foodColor
+          )
+        }
+       }
+  }
+}
+
 let draw = () => {
-  createRect(0, 0, canvas.width, canvas.height, wallInnerColor);
-  // todo
+  createRect(0, 0, canvas.width, canvas.height, "black");
   drawWalls();
+  drawFoods();
+  pacman.draw();
 };
 
 let gameInterval = setInterval(gameLoop, 1000 / fps);
@@ -116,3 +137,36 @@ let drawWalls = () => {
     }
   }
 };
+
+let createNewPacman = () => {
+  pacman = new Pacman(
+    oneBlockSize,
+    oneBlockSize,
+    oneBlockSize,
+    oneBlockSize,
+    oneBlockSize / 5
+  );
+};
+
+createNewPacman();
+gameLoop();
+
+window.addEventListener("keydown", (event) => {
+  let k = event.keyCode;
+
+  setTimeout(() => {
+    if (k == 37 || k == 65) {
+      // left
+      pacman.nextDirection = DIRECTION_LEFT;
+    } else if (k == 38 || k == 87) {
+      // up
+      pacman.nextDirection = DIRECTION_UP;
+    } else if (k == 39 || k == 68) {
+      // right
+      pacman.nextDirection = DIRECTION_RIGHT;
+    } else if (k == 40 || k == 83) {
+      // bottom
+      pacman.nextDirection = DIRECTION_BOTTOM;
+    }
+  }, 1);
+});
